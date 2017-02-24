b158406cab015738109a521f90dd5316a6201c2c
waqarjoyia <waqarahmedjoyia@live.com>
2017-02-24 14:02:54 +0500

    cleaned unnecessary code

--- test_module/apps.py
+++ test_module/apps.py
@@ -3,23 +3,3 @@ from django.apps import AppConfig
 
 class TestModuleConfig(AppConfig):
     name = 'test_module'
-
-
-from django.shortcuts import render,redirect
-from django.views.generic import View
-from django.shortcuts import render_to_response
-from django.template import RequestContext
-from django.contrib.auth import authenticate,login,logout
-from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse,HttpResponseRedirect
-from django.forms.utils import ErrorList
-from django.utils.decorators import method_decorator
-
-
-
-
-
-
-class Student_View_Group_Tests(View):
-    def get(self, request):
-        return render(request, 'student view group tests.html')
--- test_module/views.py
+++ test_module/views.py
@@ -10,23 +10,3 @@ from django.utils.decorators import method_decorator
 
 
 
-#hello
-a = 3
-from django.shortcuts import render,redirect
-from django.views.generic import View
-from django.shortcuts import render_to_response
-from django.template import RequestContext
-from django.contrib.auth import authenticate,login,logout
-from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse,HttpResponseRedirect
-from django.forms.utils import ErrorList
-from django.utils.decorators import method_decorator
-
-
-
-
-
-
-class Student_View_Group_Tests(View):
-    def get(self, request):
-        return render(request, 'student view group tests.html')
