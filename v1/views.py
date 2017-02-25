e3857ea8d9109a15c9843a1dbd41b7f5fdee2c72
arslanhashmi <bsef14a522@pucit.edu.pk>
2017-02-25 18:59:13 +0500

    login page updated 2

--- v1/views.py
+++ v1/views.py
@@ -7,13 +7,37 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse,HttpResponseRedirect
 from django.forms.utils import ErrorList
 from django.utils.decorators import method_decorator
-
+from pymongo import MongoClient
 
 
 class Login(View):
     def get(self,request):
-        return render(request,'login.html')
+        mongo = MongoClient()
+        db = mongo['dummy_school_project_v1']
+        school = db.school.find()
+        school_id = []
+        school_name = []
+        mydict={}
+        for s in school:
+          mydict[s['id']]=s['school_name']
+
+
+        print(mydict)
+        return render(request,'login.html',{'schooldata':mydict})
 
 class SignUp(View):
     def get(self,request):
         return render(request,'base.html')
+
+class check(View):
+    def get(self,request):
+        username = request.POST('username')
+        password = request.POST('password')
+        branchid = request.POST('branchid')
+        id = request.POST('schoolname')
+
+        print(username)
+        print(id)
+        return render(request,'welcome.html')
+
+
