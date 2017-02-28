0cd84fc16d73372816bad0dd3989017482fcd748
waqarjoyia <waqarahmedjoyia@live.com>
2017-02-28 15:23:48 +0500

    minor changes

--- v1/Database.py
+++ v1/Database.py
@@ -1,4 +1,6 @@
+import datetime
 from pymongo import MongoClient
+from test_module.teacher_files.teacher import Teacher
 
 
 class DataBase(object):
@@ -83,3 +85,79 @@ class StudentDataBase(DataBase):
 class TestDataBase(DataBase):
     def __init__(self):
         super().__init__()
+
+    def create_test(self,test_name, teacher_username, branch_id, school_id):
+        creation_time = datetime.datetime.now()
+        counter = self.update_teacher_document_and_set_counter_value(teacher_username, school_id, branch_id)
+        test_dict = TestDataBase.get_dict_for_creation_of_test(counter, teacher_username, school_id, branch_id,
+                                                               test_name, creation_time)
+        self.db.tests.insert(test_dict)
+        return counter
+
+
+
+    def update_teacher_document_and_set_counter_value(self, teacher_username, school_id, branch_id):
+        counter = self.db.users.find_one(Teacher.get_teacher_id_dict(teacher_username, school_id, branch_id))['counter']
+        self.db.users.update_one(Teacher.get_teacher_id_dict(teacher_username, school_id, branch_id),{
+            '$inc': {
+                'counter': 1
+            }
+        })
+        return counter
+
+    @staticmethod
+    def get_dict_for_creation_of_test(counter, teacher_username, school_id, branch_id, test_name, creation_time):
+        return {
+            'counter': counter,
+            'teacher_username': teacher_username,
+            'school_id': school_id,
+            'branch_id': branch_id,
+            'test_name': test_name,
+            'creation_date': creation_time,
+            'status': 'active',
+        }
+
+    def find_and_update_test(self,):
+        self.db.tests.find_one_and_update(
+            {'test'}
+        )
+
+
+class Test(object):
+    def __init__(self,db,teacher_username,school_id,branch_id,test_name):
+        self.database = db
+        self.db,self.teacher_username, self.school_id,self.branch_id, self.test_name = db['users'],teacher_username,school_id,\
+                                                                                       branch_id,test_name
+        self.creation_time = datetime.datetime.now()
+        self.update_teacher_document_and_set_counter_value()
+
+    def update_teacher_document_and_set_counter_value(self,):
+        self.counter = self.db.users.find_one(Teacher.get_teacher_id_dict(self.teacher_username, self.school_id,
+                                                                    self.branch_id))['counter']
+        self.db.users.update_one(Teacher.get_teacher_id_dict(self.teacher_username, self.school_id, self.branch_id),{
+            '$inc': {
+                'counter':1
+            }
+        })
+
+    def get_dict_for_creation_of_test(self):
+        return {
+            'counter': self.counter,
+            'teacher_username': self.teacher_username,
+            'school_id': self.school_id,
+            'branch_id': self.branch_id,
+            'test_name': self.test_name,
+            'creation_date': self.creation_time,
+            'status': 'active',
+        }
+
+    def create_test_document(self):
+        self.database.tests.insert(self.get_dict_for_creation_of_test())
+
+
+
+'''
+
+
+
+'''
